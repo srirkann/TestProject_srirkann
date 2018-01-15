0e13fb47d3f78a6a84298d9bdb7459e59c2cbb6a
Sriram Kannan (srirkann) <srirkann@cisco.com>
2018-01-15 23:12:53 +0530

    Test

---
+++ master_provision.sh
@@ -0,0 +1,2 @@
+apt-get -y update
+curl -L https://bootstrap.saltstack.com -o install-salt.sh
---
+++ minion_provision.sh
@@ -0,0 +1,3 @@
+apt-get -y update
+curl -L https://bootstrap.saltstack.com -o install-salt.sh
+chmod +x install-salt.sh
